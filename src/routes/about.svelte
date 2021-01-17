2a8ac1a7533cd7922247226aa0eae658bc032b50
Anshu kumar shandilya <30520154+ashandilya@users.noreply.github.com>
2021-01-18 02:29:52 +0530

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,4 +4,11 @@
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>Thank you so much for coming here.<br/>
+
+I am an Android Developer, but I have not restricted myself within Technologies. I spend more time in exploring back-end Technologies.<br/>
+
+I am also content writer, I mostly write about Android, Opensource, Back-end tech.
+https://kiwismedia.com/@ashandilya64<br/>
+
+Besides all these, We are building KiwisMedia (A Portfolio based platform where an individual get inspired by other's work and inspire the other by documenting their own works. kiwismedia.com</p>
