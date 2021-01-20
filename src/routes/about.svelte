432e81529b1aaee996e85e048572f02638cbb9c8
Anshu kumar shandilya <30520154+ashandilya@users.noreply.github.com>
2021-01-20 14:27:02 +0000

    Ebook Added

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,10 +4,10 @@
 
 <h1>About Me</h1>
 
-<p>I am an Android Developer, but I have not restricted myself within Technologies. I spend more time in exploring back-end Technologies.
-<br> We are building <b>KiwisMedia (A Portfolio based platform where an individual get inspired by other's work and inspire the other by documenting their own works. kiwismedia.com</b>
+<p>I am an Android Developer, but I have not restricted myself within Technologies. I spend more time in exploring back-end Technologies.<br>
+<b> We are building <b>KiwisMedia (A Portfolio based platform where an individual get inspired by other's work and inspire the other by documenting their own works. kiwismedia.com</b>
 
 I am also content writer, I mostly write about Android, Opensource, Back-end tech.
 <br>
-<h2>Check Out my KiwisMedia Work Portfolio</h2> 
-https://kiwismedia.com/@ashandilya64</p>
+<a href = "https://kiwismedia.com/@ashandilya64">Check Out my KiwisMedia Work Portfolio</a> 
+</p>
